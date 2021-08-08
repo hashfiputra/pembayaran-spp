b0b43c87d3613b25433cdabb9def1f81c6693211
hashfiputra <hashfiputra0@gmail.com>
2021-08-08 14:44:25 +0700

    Daftar dari view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,9 +33,9 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Lupa password?') }}
+                @if (Route::has('register'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('Belum Daftar?') }}
                     </a>
                 @endif
 
